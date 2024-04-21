8bb1a82df7a3423cfc2754492228f7efa4e8bead
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-21 12:46:40 +1000

    Billing.jsx completed

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -18,9 +18,7 @@ const Billing = () => (
         invoicing
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
