0ae1fcd421efaca156fb12345424b1e81a92af1b
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-21 12:45:48 +1000

    Footer competed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => (
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo}
-          alt="hoobank"
+          alt="ditiae"
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+          A new way to make payments. Our services are easy, reliable and secure.
         </p>
       </div>
 
@@ -41,7 +41,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+      <div>Completed for non-commerical purposes in 2024 || Chloe O</div>
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
